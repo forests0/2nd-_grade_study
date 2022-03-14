6d2676cfaf34bd9935013a3dfe37fe67c6bae8ff
남가연 <forestser00@gmail.com>
2022-03-14 16:18:24 +0900

    C++ bool, namespace

---
+++ C++/s.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+void Sbyval(int ref1, int ref2) {
+    int temp = ref1;
+    ref1 = ref2;
+    ref2 = temp;
+}
+
+void Sbyref(int *ref1, int *ref2) {
+    int temp = *ref1;
+    *ref1 = *ref2;
+    *ref2 = temp;
+}
+
+int main(void) {
+    int val1 = 10;
+    int val2 = 20;
+
+    sbyval();
+
+    sbyref();
+}
