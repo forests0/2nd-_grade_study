01a0ecc7fdd1f6748ba26531580dee66dcf9cb7f
Forests <99336909+forests0@users.noreply.github.com>
2022-11-15 08:10:42 +0900

    C++ 플로이드 워셔

--- C++/Floyd-Warshall.cpp
+++ C++/Floyd-Warshall.cpp
@@ -1,45 +1,43 @@
 #include <iostream>
 using namespace std;
-#define INF 9999999
 
-int n,m;
+#define INF 999999999
+
+int n, m;
 int graph[501][501];
 
 void init_graph() {
-  for (int i=1; i<=n; i++){
-	for (int j=1; j<=n; j++){
-	  graph[i][j] = INF;
-	  if (i==j){
-  		graph[i][j] = 0;
-      }
+	for(int i = 0 ; i <= n ; i++) {
+		for(int j = 0 ; j <= n; j++)
+			graph[i][j] = INF;
+		graph[i][i] = 0;
 	}
-  }
 }
 
-int main() {
-  cin>>n>>m;
-  init_graph();
-  for(int i = 1; i <= m; i++) {
-    int start, end, cost;
-    cin>>start>>end>>cost;
-    graph[start][end] = cost;
-  }
-
-  for(int i = 1; i <= n; i++) {
-    for(int j = 1; j <= n; j++) {
-      for(int k = 1; k <= n; k++) {
-        graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k]);
-      }
-    }
-  }
-
-  for(int i = 1; i <= n; i++) {
-    for(int j = 1; j <= n; j++) {
-      if(graph[i][j] == INF) {
-        cout<< -1 << ' ';
-      }
-      cout << graph[i][j] << ' ';
-    }
-    cout << endl;
-  }
+int main(void) {
+	cin >> n >> m;
+	init_graph();
+	for(int i = 0 ; i < m ; i++) {
+		int start, end, cost;
+		cin >> start >> end >> cost;
+		graph[start][end] = cost;
+	}
+	
+	// 플로이드 워셜
+	for(int i = 1 ; i <= n ; i++)
+		for(int j = 1 ; j <= n ; j++)
+			for(int k = 1 ; k <= n ; k++)
+				graph[j][k] = min(graph[j][k],graph[j][i] + graph[i][k]);
+	
+	
+	for(int i = 1 ; i <= n ; i++) {
+		for(int j = 1 ; j<= n ; j++) {
+			if(graph[i][j]==INF)
+				cout << -1 << ' ';
+			else
+				cout << graph[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	return 0;
 }
